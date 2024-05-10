840fd05331eee97bf9c8df027b0b73af1f45e79f
Tomasscalisse12 <tomasscalissedefreitas@gmail.com>
2024-05-10 10:48:33 -0300

    nota-em-python

---
+++ app.py
@@ -0,0 +1,14 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média. 
+
+n1= float(input("primeira nota do aluno :"))
+n2= float(input("segunda nota :"))
+media=(n1+n2)/2
+print(" primeira prova {}\n nota da segunda prova{}\n sua media é {:.1}".format(n1,n2,media))
+
+#segundo metodo
+
+n3=float(input("primeira nota"))
+n4=float(input("segunda nota"))
+print("primeira prova {} \n segunda prova {}\n nota final {:.1}".format(n3,n4,(n3+n4)/2))
+
+
